c5f2028a8df9025da2141c4b814d119c7f8781ef
Justin <jalirkan@gmail.com>
2021-06-22 19:48:25 -0400

    Added everyone's info to read me and reformatted tweet_reachout py file

--- app/tweet_reachout.py
+++ app/tweet_reachout.py
@@ -1,24 +1,30 @@
 from app.twitter_bot import twitter_reply
 
-#Dummy class for a ticket to minitor replies, is not currently being used
+
+# Dummy class for a ticket to monitor replies, is not currently being used
+
+
 class TweetReachOut:
 
-    def __init__(self, tweet, open=True,  responded=True):
+    def __init__(self, tweet, open=True, responded=True):
         self.initial_tweet_id = tweet.id_str
         self.open = open
-        self.conversation_log = {tweet.id_str: {"username": tweet.user.screen_name, "tweet_text": tweet.full_text, "replies": []}}
+        self.conversation_log = {
+            tweet.id_str: {"username": tweet.user.screen_name, "tweet_text": tweet.full_text, "replies": []}}
         self.responded = responded
 
     def send_response(self, id, username, text):
         sent_tweet = twitter_reply(id, username, text)
         self.responded = True
         self.conversation_log[id]['replies'].append(sent_tweet.id_str)
-        self.conversation_log[sent_tweet.id_str] = {"username": sent_tweet.user.screen_name, "tweet_text": sent_tweet.full_text, "replies": []}
+        self.conversation_log[sent_tweet.id_str] = {"username": sent_tweet.user.screen_name,
+                                                    "tweet_text": sent_tweet.full_text, "replies": []}
         return "Response sent"
 
     def did_respond(self, tweet):
         self.conversation_log[tweet.in_reply_to_status_id_str]['replies'].append(tweet.id_str)
-        self.conversation_log[str(tweet.id)] = {"username": tweet.user.screen_name, "tweet_text": tweet.full_text, "replies": []}
+        self.conversation_log[str(tweet.id)] = {"username": tweet.user.screen_name, "tweet_text": tweet.full_text,
+                                                "replies": []}
         self.responded = False
 
     def print_log(self):
