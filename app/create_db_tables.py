5613625331bbb68b1f64209119c2984326e1239c
Justin <jalirkan@gmail.com>
2021-06-23 15:50:12 -0400

    made schema consistent with db tables

--- app/create_db_tables.py
+++ app/create_db_tables.py
@@ -1,30 +1,26 @@
 """ This file holds the schemas to create new tables on database, if necessary.
-To create a table change the table value in function on line 89 and run file.
+To create a table change the table value in function on line 51 and run file.
 This code should not have to be run unless new (empty) tables have to be generated.
 """
 import os
 
 import psycopg2
 from dotenv import load_dotenv
-load_dotenv()
-
 
 def initialize_police_table():
     load_dotenv()
     police_table = """CREATE TABLE IF NOT EXISTS police_force (
-            id SERIAL PRIMARY KEY NOT NULL,
-            dates TIMESTAMP,
-            links TEXT,
-            case_id TEXT,
-            city TEXT,
-            state TEXT,
-            lat FLOAT,
-            lon FLOAT,
-            title TEXT,
-            description TEXT,
-            tags TEXT,
-            force_rank TEXT,
-            confidence FLOAT
+    id SERIAL PRIMARY KEY NOT NULL,
+    dates TIMESTAMP,
+    links TEXT,
+    case_id TEXT,
+    city TEXT,
+    state TEXT,
+    title TEXT,
+    description TEXT,
+    tags TEXT,
+    force_rank TEXT,
+    confidence TEXT
         );"""
 
     pi_table = """CREATE TABLE IF NOT EXISTS incidents (
@@ -35,12 +31,10 @@ def initialize_police_table():
     user_description TEXT,
     twitter_text TEXT,
     force_rank TEXT,
-    confidence FLOAT,
+    confidence TEXT,
     tags TEXT,
     city TEXT,
     state TEXT,
-    lat FLOAT,
-    lon FLOAT,
     twitterbot_tweet_id TEXT,
     responses TEXT
           );"""
