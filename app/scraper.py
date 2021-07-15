6111f2406d215b9b2961ea483fa11d8e08084ed8
Eric <ericyeonpark@gmail.com>
2021-07-15 13:28:20 -0700

    cleaned code in py files

--- app/scraper.py
+++ app/scraper.py
@@ -50,7 +50,7 @@ def update_twitter_data():
     conn.close()
 
     db = dataset.connect(os.getenv("DB_URL"))
-    table = db["twitter_incidents_new"]
+    table = db["incidents"]
     conn = psycopg2.connect(os.getenv("DB_URL"))
     curs = conn.cursor()
     conn.commit()
@@ -72,37 +72,19 @@ def update_twitter_data():
 
         if conditions:
 
-            print('status.full_text: ', status.full_text) #DELETE DELETE DELETE DELETE DELETE
-            # category = get_rank_of_force(status.full_text).text  # original model pulled from api requiring .text
             category = get_rank_of_force(status.full_text)  # This runs the text of the Tweet through the model
-            #print('category: ', category) # DELETE DELETE DELETE DELETE
-            #print('category type: ', type(category)) # DELETE DELETE DELETE DELETE
 
             dupe_check.append(status.id_str)  # Keeps track
 
             if category != '{"detail":"Not Found"}':
                 category_splitted = category.split(': ')
-                print('category splitted', category_splitted) # DELETE DELETE DELETE DELETE
                 rank_confidence = category_splitted[1].split(', ')[1].replace('%', '')
-                print('rank_confidence: ', rank_confidence) # DELETE DELETE DELETE DELETE
                 rank_int = int(category_splitted[1].split(', ')[0])  # Gets rank integer for processing
-                print('rank_int: ', rank_int) # DELETE DELETE DELETE DELETE
-                # print('rank_int: ', type(rank_int))
-
-                '''OLD CODE'''
-                #rank_confidence = category.split(': ')[1].replace('%', '').replace('"', '')
-                #print('rank_confidence: ', rank_confidence) # DELETE DELETE DELETE DELETE
-                #rank_int = int(category.split(': ')[0][-1])  # Gets rank integer for processing
-                print('rank_int: ', rank_int) # DELETE DELETE DELETE DELETE
-                
             else:
-                print('else statement passed') # DELETE DELETE DELETE DELETE DELETE
                 rank_int = 0
-
             
             if rank_int > 1:
 
-                print('if statement passed') # DELETE DELETE DELETE DELETE DELETE
                 tweet_id = status.id_str
                 date_created = status.created_at
                 user_name = status.user.screen_name
