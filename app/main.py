88bc6514c96206902e47a082becdc36befe52e15
Eric <ericyeonpark@gmail.com>
2021-07-23 14:23:16 -0700

    added an endpoint to return a prediction of police force and violence

--- app/main.py
+++ app/main.py
@@ -7,10 +7,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_utils.tasks import repeat_every
 
-from app import db, twitter, reddit
 from app.scraper import update_twitter_data
-from app.helper_funcs import check_new_items, preprocess_new_data, load_data, insert_data
-from app.create_db_tables import initialize_police_table
+from app.helper_funcs import get_rank_of_force
 
 import logging
 
@@ -26,7 +24,7 @@ DS API for the Human Rights First Blue Witness Dashboard
 To use these interactive docs:
 - Click on an endpoint below
 - Click the **Try it out** button
-- Edit the Request body or any parameters
+- Edit the user_input field to predict whether text input contains police violence
 - Click the **Execute** button
 - Scroll down to see the Server response Code & Details
 """
@@ -36,6 +34,11 @@ app = FastAPI(
     description=description,
     docs_url='/',
 )
+
+
+@app.get("/frankenbert/{user_input}")
+async def frankenbert(user_input):
+    return get_rank_of_force(user_input)
 
 
 @app.on_event('startup')
