e1bf8b09ef6933b5c871a07391d152c1a07b9fff
BOsterbuhr <brodyosterbuhr@gmail.com>
2021-08-11 17:32:06 -0500

    Add more DM functions

---
+++ app/scrap_paper.py
@@ -0,0 +1,40 @@
+quick_reply_use_of_force = [
+    {
+        "label": "Rank 1",
+        "description": "Non-violent Police Presence.",
+        "metadata": "force_rank_1"
+    },
+    {
+        "label": "Rank 2",
+        "description": "Open Handed (Arm Holds & Pushing)",
+        "metadata": "force_rank_2"
+    },
+    {
+        "label": "Rank 3",
+        "description": "Blunt Force Trauma (Batons & Shields)",
+        "metadata": "force_rank_3"
+    },
+    {
+        "label": "Rank 4",
+        "description": "Chemical & Electric Weapons (Tasers & Pepper Spray)",
+        "metadata": "force_rank_4"
+    },
+    {
+        "label": "Rank 5",
+        "description": "Lethal Force (Guns & Explosives)",
+        "metadata": "force_rank_5"
+    }
+]
+
+confirmation_reply = [
+    {
+        'label': 'Yes',
+        'description': 'Yes I can provide more information',
+        'metadata': 'confirm_yes'
+    },
+    {
+        'label': 'No',
+        'description': 'No I can\'t provide more information',
+        'metadata': 'confirm_no'
+    }
+]
---
+++ app/tweep_dm.py
@@ -0,0 +1,195 @@
+import json
+import os
+from typing import Dict, List
+
+import requests
+import tweepy
+from dotenv import load_dotenv
+from requests_oauthlib import OAuth1Session
+
+from scrap_paper import confirmation_reply, quick_reply_use_of_force
+
+load_dotenv()
+
+auth = tweepy.OAuthHandler(os.getenv("CONSUMER_KEY"),
+                           os.getenv("CONSUMER_SECRET"))
+auth.set_access_token(os.getenv("ACCESS_KEY"), os.getenv("ACCESS_SECRET"))
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+manual_twitter_auth = OAuth1Session(os.getenv("CONSUMER_KEY"),
+                                    os.getenv("CONSUMER_SECRET"),
+                                    os.getenv("ACCESS_KEY"),
+                                    os.getenv("ACCESS_SECRET"))
+
+# Just my own for now
+blue_witness_recipient_id = 271825982
+
+# This will change and instead pull the welcome messages from twitter but for now storing locally for ease of use
+list_of_welcome_messages = [
+    {
+        'welcome_message': {
+            'id': '1424836133582774286',
+            'created_timestamp': '1628542381078',
+            'message_data': {
+                'text': 'new message',
+                'entities': {
+                    'hashtags': [],
+                    'symbols': [],
+                    'user_mentions': [],
+                    'urls': []
+                }
+            },
+            'source_app_id': '21602950',
+            'name': 'new name'
+        },
+        'apps': {
+            '21602950': {
+                'id': '21602950',
+                'name': 'bluewitness1'
+            }
+        }
+    },
+    {
+        'welcome_message': {
+            "id": "1424835217869709320",
+            "created_timestamp": "1628542162755",
+            "message_data": {
+                "text": "new message",
+                "entities": {
+                    "hashtags": [],
+                    "symbols": [],
+                    "user_mentions": [],
+                    "urls": []
+                },
+                "quick_reply": {
+                    "type": "options",
+                    "options": [
+                        {
+                            "label": "Rank 1",
+                            "metadata": "force_rank_1",
+                            "description": "Non-violent Police Presence."
+                        },
+                        {
+                            "label": "Rank 2",
+                            "metadata": "force_rank_2",
+                            "description": "Open Handed (Arm Holds & Pushing)"
+                        },
+                        {
+                            "label": "Rank 3",
+                            "metadata": "force_rank_3",
+                            "description": "Blunt Force Trauma (Batons & Shields)"
+                        },
+                        {
+                            "label": "Rank 4",
+                            "metadata": "force_rank_4",
+                            "description": "Chemical & Electric Weapons (Tasers & Pepper Spray)"
+                        },
+                        {
+                            "label": "Rank 5",
+                            "metadata": "force_rank_5",
+                            "description": "Lethal Force (Guns & Explosives)"
+                        }
+                    ]
+                }
+            }
+        }
+    }
+]
+
+list_of_A_B_txts = ['''Hi! I am a bot for Blue Witness, a project by the
+                    Human Rights First. We noticed a tweet that may involve
+                    police misconduct, can you confirm this?''',
+                    ]
+
+list_of_quick_replies = [quick_reply_use_of_force, confirmation_reply]
+
+
+def create_welcome_message(name: str,
+                           msg_txt: str,
+                           quick_replies: List[Dict] = None) -> Dict:
+
+    url = "https://api.twitter.com/1.1/direct_messages/welcome_messages/new.json"
+    if quick_replies:
+        payload = json.dumps({
+            "welcome_message": {
+                "name": name,
+                "message_data": {
+                    "text": msg_txt,
+                    "quick_reply": {
+                        "type": "options",
+                        "options": quick_replies
+                    }
+                }
+            }
+        })
+    else:
+        payload = json.dumps({
+            "welcome_message": {
+                "name": name,
+                "message_data": {
+                    "text": msg_txt
+                }
+            }
+        })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = manual_twitter_auth.post(url, headers=headers, data=payload)
+
+    welcome_response = json.loads(response.text)
+    list_of_welcome_messages.append(welcome_response)
+
+
+def reply_to_tweet(tweet_id,
+                   reply_text,
+                   welcome_message_name,
+                   blue_witness_recipient_id):
+    """Build the inital response tweet that will be used to reply to the user's incident tweet"""
+    for welcome_message in list_of_welcome_messages:
+        if welcome_message['name'] == welcome_message_name:
+            welcome_message_id = welcome_message['welcome_message']['id']
+    dm_link = f'https://twitter.com/messages/compose?recipient_id={blue_witness_recipient_id}&welcome_message_id={welcome_message_id}'
+    reply_message = f"{reply_text} \n {dm_link}"
+    api.update_status(reply_message, in_reply_to_status_id=tweet_id,
+                      auto_populate_reply_metadata=True)
+
+
+def get_user_id_from_tweet(tweet_id):
+    tweet = api.get_status(tweet_id)
+    user_id = tweet.user.id
+    return user_id
+
+
+def get_initial_dms(user_id: List[str]) -> List[Dict]:
+    """Function to get DMs sent from button in tweet"""
+    dms = api.list_direct_messages()
+    dm_list = []
+    for dm in dms:
+        if dm.message_create['sender_id'] in user_id:
+            dm_list.append({
+                "dm_id": dm.id,
+                "time_created": dm.created_timestamp,
+                "welcome_message": dm.initiated_via['welcome_message_id'],
+                "sender_id": dm.message_create['sender_id'],
+                "text": dm.message_create['message_data']['text'],
+                "quick_reply_response": dm.message_create['message_data']['quick_reply_response']['metadata']
+            })
+    return dm_list
+
+
+def send_clarification_dm(dm_id, A_B_txt, welcome_message_name, quick_replies: List[Dict] = None, ):
+    """ Sends DM to twitter user_id with quick reply options to clarify if tweet is police misconduct. """
+    user_id = None  # Use dm_id to search db for recipient_id
+    
+    api.send_direct_message(user_id, A_B_txt, quick_replies)
+
+
+def get_response_dms(dm_id_list: List[Dict]) -> List[Dict]:
+    """ Function to get DMs after initial response """
+    # Need to get a list of dm_ids from database
+    pass
+
+
+dms_to_check = ["204178035", "1424579675645378561"]
+print(get_initial_dms(dms_to_check))
