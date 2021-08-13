a864dcd81be2d0a07adc7d523173b9ff999f36a8
mbenbanaste <mbenbanaste@gmail.com>
2021-08-13 08:52:05 -0700

    fixed a bug in the API regarding symbols in URLs in GET requests

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ from typing import List, Dict
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_utils.tasks import repeat_every
+from pydantic import BaseModel
 
 from app.scraper import deduplicate, frankenbert_rank, scrape_twitter, DB
-
 
 description = """
 DS API for the Human Rights First Blue Witness Dashboard
@@ -19,18 +19,23 @@ To use these interactive docs:
 - Scroll down to see the Server response Code & Details
 """
 
+
+class InputString(BaseModel):
+    text: str
+
+
 app = FastAPI(
     title='Labs 36 HRF BW DS API',
     description=description,
     docs_url='/',
-    version="0.36.6",
+    version="0.37.1",
 )
 
 
-@app.get("/frankenbert/{user_input}")
-async def frankenbert(user_input: str):
+@app.post("/frankenbert")
+async def frankenbert(user_input: InputString):
     """ Prediction endpoint, for testing and demonstration purposes """
-    rank, conf, *_ = frankenbert_rank(user_input)
+    rank, conf, *_ = frankenbert_rank(user_input.text)
     return {"Rank": rank, "Confidence": conf}
 
 
@@ -45,8 +50,9 @@ async def view_data():
 
 
 
+
 @app.on_event("startup")
-@repeat_every(seconds=60*60*4)
+@repeat_every(seconds=60 * 60 * 4)
 async def update():
     """ 1. scrape twitter for police use of force
         2. deduplicate data based on tweet id
@@ -76,6 +82,7 @@ app.add_middleware(
 
 if __name__ == '__main__':
     import uvicorn
+
     """ To run locally, use this command in the terminal:
     python3 -m app.main
     """
