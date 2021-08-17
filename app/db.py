f7603542e967558d6bfa4aa093cdb20206ba8b72
BOsterbuhr <brodyosterbuhr@gmail.com>
2021-08-17 04:42:06 -0500

    Incorporating database structure

--- app/db.py
+++ app/db.py
@@ -39,6 +39,14 @@ class Database(object):
 
         return data
 
+    def get_conversation_root(self, root_id: int):
+        """ Get conversation with a specific root_tweet_id """
+        with self.Sessionmaker() as session:
+            query = select(Conversations).where(Conversations.root_tweet_id == root_id)
+            conversations_data = session.execute(query)
+
+        return [i[0] for i in conversations_data.fetchall()]
+
 
     def get_conversation_root(self, root_id: int):
         """ Get conversation with a specific tweet_id """
