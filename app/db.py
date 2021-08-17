2a0ce7dd6203cde6c1b6d957f8f9902f35cfda68
BOsterbuhr <brodyosterbuhr@gmail.com>
2021-08-17 04:42:06 -0500

    Incorporating database structure

--- app/db.py
+++ app/db.py
@@ -8,12 +8,12 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine, select, insert, update, func, inspect
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from app.models import ForceRanks, Base
+from app.models import ForceRanks, Conversations, Form, Base
 
 
 load_dotenv()
 db_url = os.getenv('DB_URL')
-table_name = 'force_ranks'
+
 
 
 class Database(object):
@@ -44,8 +44,16 @@ class Database(object):
 
         return data
 
+    def get_conversation_root(self, root_id: int):
+        """ Get conversation with a specific root_tweet_id """
+        with self.Sessionmaker() as session:
+            query = select(Conversations).where(Conversations.root_tweet_id == root_id)
+            conversations_data = session.execute(query)
 
-    def load_data(self):
+        return [i[0] for i in conversations_data.fetchall()]
+
+
+    def load_data_force_ranks(self):
         """ gets all data from force_ranks"""
         with self.Sessionmaker() as session:
             query = select(ForceRanks)
@@ -54,7 +62,7 @@ class Database(object):
         return force_ranks_data
 
 
-    def load_tweet_ids(self):
+    def load_tweet_ids_force_ranks(self):
         """ gets all tweet_ids from force_ranks """
         with self.Sessionmaker() as session:
             query = select(ForceRanks.tweet_id)
@@ -63,38 +71,93 @@ class Database(object):
         return force_ranks_data
 
 
-    def insert_data(self, data: List[Dict]):
+    def insert_data_force_ranks(self, data: List[Dict]):
         """ inserts data into force_ranks """
         with self.Sessionmaker() as session:
             last = select(func.max(ForceRanks.incident_id))
             last_value = session.execute(last).fetchall()[0][0]
             for datum in range(len(data)):
+                if last_value is None:
+                    last_value = 0
                 last_value += 1
                 data[datum]['incident_id'] = last_value
-                if type(data[datum]['confidence']) != float:
+                if type(data[datum]['confidence']) != float and data[datum]['confidence'] != None:
                     data[datum]['confidence'] = data[datum]['confidence'].item()
                 obj = ForceRanks(**data[datum])
                 session.add(obj)
                 session.commit()
 
 
+    def update_force_rank(self, data, tweet_id):
+        """ updates force rank columns of matching tweet_id """
+        query = (
+            update(ForceRanks).
+            where(ForceRanks.tweet_id == str(tweet_id)).
+            values(**data)
+        )
+
+        with self.Sessionmaker() as session:
+            session.execute(query)
+            session.commit()
+
+
+    def update_conversations(self, data, tweet_id):
+        """ updates conversations columns of matching tweet_id """
+        query = (
+            update(Conversations).
+            where(Conversations.root_tweet_id == str(tweet_id)).
+            values(**data)
+        )
+
+        with self.Sessionmaker() as session:
+            session.execute(query)
+            session.commit()
+
+
     def initialize_ranks_table(self):
-        """ creates table if not exists """
+        """ creates force_ranks table if not exists """
         insp = inspect(self.engine)
-        if insp.has_table(table_name) == False:
+        if insp.has_table("force_ranks") == False:
             ForceRanks.__table__.create(self.engine)
 
 
-    def reset_table(self):
+    def initialize_conversations_table(self):
+        """ creates conversations table if not exists """
+        insp = inspect(self.engine)
+        if insp.has_table("conversations") == False:
+            Conversations.__table__.create(self.engine)
+
+    def initialize_form_table(self):
+        """ creates form table if not exists """
+        insp = inspect(self.engine)
+        if insp.has_table("form") == False:
+            Form.__table__.create(self.engine)
+
+    def reset_force_ranks(self):
         """ DANGER! this will delete all data in the table!!! """
         check = input('Are you sure? This will delete all table data (Y/N):')
         if check == 'Y':
             insp = inspect(self.engine)
-            if insp.has_table(table_name) == True:
+            if insp.has_table("force_ranks") == True:
                 ForceRanks.__table__.drop(self.engine)
             self.initialize_ranks_table()
         elif check == 'N':
             pass
         else:
             print('Please answer Y or N')
+
+
+    def reset_conversations(self):
+        """ DANGER! this will delete all data in the table!!! """
+        check = input("Are you sure? This will delete all table data (Y/N):")
+        if check == 'Y':
+            insp = inspect(self.engine)
+            if insp.has_table("conversations") == True:
+                Conversations.__table__.drop(self.engine)
+            self.initialize_conversations_table()
+        elif check == 'N':
+            pass
+        else:
+            print('Please answer Y or N')
+
 
--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
-from sqlalchemy import Column, Date, Float, Integer, String, Table
+from sqlalchemy import Column, Date, Float, Integer, String, Table, Boolean
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.schema import ForeignKey
 
 Base = declarative_base()
 
@@ -22,20 +24,65 @@ class DirectMessages(Base):
     quick_reply_response = Column(String)
 
 
+class Form(Base):
+
+    __tablename__ = 'form'
+
+    id = Column(Integer, primary_key=True, ForeignKey='incident.id', nullable=False)
+    form_type = Column(String, nullable=False)
+    source_tweet_id = Column(String, nullable=False)
+    response_city = Column(String, default=None)
+    response_state = Column(String, default=None)
+    response_force_rank = Column(Integer, default=None)
+    response_YN_force = Column(Boolean, default=None)
+    force_ranks = relationship('ForceRanks', back_populates='form')
+
 class Conversations(Base):
 
-    __tablename__ = "conversations"
+	__tablename__ = "conversations"
 
-    id = Column(Integer, primary_key=True)
-    root_tweet_id = Column(String)
-    sent_tweet_id = Column(String)
-    received_tweet_id = Column(String)
-    in_reply_to_id = Column(String)
-    tweeter_id = Column(String)
-    conversation_status = Column(Integer)
-    tweet_text = Column(String)
-    checks_made = Column(Integer)
-    reachout_template = Column(String)
+	id = Column(Integer, primary_key=True)
+	root_tweet_id = Column(String)
+	form = Column(Integer)
+	root_tweet_city = Column(String(255))
+	root_tweet_state = Column(String(255))
+	root_tweet_lat = Column(Float)
+	root_tweet_long = Column(Float)
+	root_tweet_date = Column(Date)
+	root_tweet_force_rank = Column(String(255), default=None)
+	sent_tweet_id =	Column(String)
+	received_tweet_id = Column(String)
+	in_reply_to_id = Column(String)
+	tweeter_id = Column(String)
+	conversation_status = Column(Integer)
+	tweet_text = Column(String)
+	checks_made = Column(Integer)
+	reachout_template = Column(String)
+	isChecked = Column(Boolean)
+
+
+	def __repr__(self):
+		return(
+			"id:{}, root_tweet_id:{}, form:{}, root_tweet_city:{}, root_tweet_state:{}, root_tweet_lat:{}, root_tweet_long:{}, root_tweet_date:{}, root_tweet_force_rank:{}, sent_tweet_id:{}, received_tweet_id:{}, in_reply_to_id:{}, tweeter_id:{}, conversation_state:{}, tweet_text:{}, checks_made:{}, reachout_template:{}, isChecked:{}").format(
+			self.id,
+			self.root_tweet_id,
+			self.form,
+			self.root_tweet_city,
+			self.root_tweet_state,
+			self.root_tweet_lat,
+			self.root_tweet_long,
+			self.root_tweet_date,
+			self.root_tweet_force_rank,
+			self.sent_tweet_id,
+			self.received_tweet_id,
+			self.in_reply_to_id,
+			self.tweeter_id,
+			self.conversation_status,
+			self.tweet_text,
+			self.checks_made,
+			self.reachout_template,
+			self.isChecked
+			)
 
 
 class ForceRanks(Base):
@@ -57,6 +104,7 @@ class ForceRanks(Base):
     confidence = Column(Float)
     tags = Column(String(255))
     src = Column(String(8000))
+    form = relationship("Form", back_populates="force_ranks")
 
     def __repr__(self):
         return (
