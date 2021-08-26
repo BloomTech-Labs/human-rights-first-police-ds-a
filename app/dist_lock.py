8774034ac262be3270d01308219b55cd8ebb73af
RowenWitt <rew4200@protonmail.com>
2021-08-26 14:53:28 -0700

    adds redis distributed lock functionality, local and deployed tests passed

--- app/dist_lock.py
+++ app/dist_lock.py
@@ -19,3 +19,29 @@ def ping():
         logging.info('Connected!')
     except Exception as ex:
         logging.error('Failed to connect, terminating.')
+
+
+class DistributedLock(object):
+
+    def __init__(self, host=os.getenv('REDIS_HOST'), port=os.getenv('REDIS_PORT')):
+        self.conn = redis.Redis(host=host, port=port, db=0)
+
+    def lock(self, key, exp):
+        res = self.conn.set(key, 1, exp, nx=True)
+        if res == True:
+            return True
+
+    def unlock(self, key):
+        res = self.conn.delete(key)
+        if res > 0:
+            return True
+
+        return False
+
+lock = DistributedLock()
+# print("unlocking", lock.unlock("rowentest"))
+# print("locking", lock.lock("rowentest", 5))
+# print("locking", lock.lock("rowentest", 5))
+# print("unlocking", lock.unlock("rowentest"))
+# print("locking", lock.lock("rowentest", 5))
+# print("locking", lock.lock("rowentest", 5))
